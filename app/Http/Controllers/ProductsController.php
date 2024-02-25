d809df00fe4f0d084d1b93b677438b6441786591
putrakhamdani <=>
2024-02-25 16:09:40 +0700

    Update ProductControllers

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
-    //
+    public function baby() {
+        return view('Products.baby-kid');
+    }
+    public function beauty() {
+        return view('Products.beauty-health');
+    }
+    public function food() {
+        return view('Products.food-baverage');
+    }
+    public function homecare() {
+        return view('Products.home-care');
+    }
 }
