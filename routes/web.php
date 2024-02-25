b728bb5d10ff42a827580d4d2ee8addd6bb9a42e
putrakhamdani <=>
2024-02-25 16:43:28 +0700

    membuat routing

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,18 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PenjualanController;
+use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/', [HomeController::class, 'home']);
+Route::get('/penjualan', [PenjualanController::class, 'penjualan']);
+Route::get('/user/{id}/name/{name}', [UserController::class, 'user']);
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('category')->group(function(){
+    Route::get('/food-beverage', [ProductsController::class, 'food']);
+    Route::get('/beauty-health', [ProductsController::class, 'beauty']);
+    Route::get('/home-care', [ProductsController::class, 'homecare']);
+    Route::get('/baby-kid', [ProductsController::class, 'baby']);
 });
